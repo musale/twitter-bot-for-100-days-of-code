208ef8bd2f1321f7f526331326d42307cc7f231e
musale <musale@focusmobile.com>
2017-01-24 10:42:55 +0300

    got the set up to fetch commits from the github api

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-#!/usr/bin/env python3
+"""run file which houses the bot logic."""
+import json
+
+import requests
+
+response = requests.get("https://api.github.com/repos/musale/100-days-of-code/commits")
+
+commit = json.loads(response.content)[0]['commit']['message']
+logs = open("logs.txt", "r")
+previous_commit = logs.readline()
+
+if commit == previous_commit:
+    # No new commit, do something weird
+    print "No new commit mate"
+else:
+    # New commit, tweet it!
+    print "Tweeting %s" % (commit,)
+    new_log = open("logs.txt", "w")
+    new_log.write(commit)
+    new_log.close()
